9bc43e1a9e8070bb1617fde68b74794a523c0578
Hernanda2829 <hernanda.pramudita@students.amikom.ac.id>
2025-10-01 08:22:58 +0700

    update code error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -25,28 +24,3 @@ def create_app():
         return {"message": "server jalan"}
 
     return app
-
-
-app = create_app()
-
-# pastikan folder ada
-os.makedirs("uploads", exist_ok=True)
-os.makedirs("data/submissions", exist_ok=True)
-os.makedirs("data/reports", exist_ok=True)
-=======
-from fastapi import FastAPI
-from app.routes import router
-import os
-
-def create_app():
-    app = FastAPI(title="Mini Project - Backend Evaluator")
-    app.include_router(router)
-    return app
-
-app = create_app()
-
-# ensure uploads and data folders
-os.makedirs("uploads", exist_ok=True)
-os.makedirs("data", exist_ok=True)
-os.makedirs("data/submissions", exist_ok=True)
->>>>>>> 59a67804aec833ff347d39cfbdf8cc78a0077eb0
